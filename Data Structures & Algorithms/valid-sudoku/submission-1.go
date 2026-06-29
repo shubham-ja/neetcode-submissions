c1e39ed206f549f72f90aff2c8db7cb5e4119560
Shubham Jain <jainshubham1293@gmail.com>
2026-06-29 23:41:18 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.go
@@ -0,0 +1,27 @@
+func isValidSudoku(board [][]byte) bool {
+    rows := make([]int, 9)
+    cols := make([]int, 9)
+    squares := make([]int, 9)
+    for r := 0; r < 9; r++ {
+        for c := 0; c < 9; c++ {
+			if board[r][c] == '.' {
+                continue
+            }
+
+            val := board[r][c] - '1'
+			
+            bit := 1 << val
+            squareIdx := (r/3)*3 + c/3
+            if rows[r]&bit != 0 || cols[c]&bit != 0 ||
+               squares[squareIdx]&bit != 0 {
+                return false
+            }
+
+            rows[r] |= bit
+            cols[c] |= bit
+            squares[squareIdx] |= bit
+        }
+    }
+
+    return true
+}
